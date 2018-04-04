8e2869b20a0757f6a4eba9c6b89317557177f488
Neha_headerlabs <neha@headerlabs.com>
2018-04-04 10:48:51 +0530

    Initial Commit

---
+++ WebKitSDK/WebView.swift
@@ -0,0 +1,36 @@
+//
+//  WebView.swift
+//  WebKitSDK
+//
+//  Created by Neha on 4/4/18.
+//  Copyright © 2018 Neha. All rights reserved.
+//
+
+import Foundation
+
+public class WebView {
+    
+    //1.
+    private var isDebug: Bool!
+    
+    //2.
+    public init() {
+        self.isDebug = false
+    }
+    
+    //3.
+    public func setup(isDebug: Bool) {
+        self.isDebug = isDebug
+        print("Project is in Debug mode: \(isDebug)")
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+    }
+    
+}
