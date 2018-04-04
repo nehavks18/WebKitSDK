6e31044e2b72cbb6d6ad353d845c491980263b25
Neha_headerlabs <neha@headerlabs.com>
2018-04-04 09:32:29 +0530

    Initial Commit

---
+++ WebKitSDK/WebKitSDK.h
@@ -0,0 +1,19 @@
+//
+//  WebKitSDK.h
+//  WebKitSDK
+//
+//  Created by Neha on 4/4/18.
+//  Copyright © 2018 Neha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebKitSDK.
+FOUNDATION_EXPORT double WebKitSDKVersionNumber;
+
+//! Project version string for WebKitSDK.
+FOUNDATION_EXPORT const unsigned char WebKitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebKitSDK/PublicHeader.h>
+
+
